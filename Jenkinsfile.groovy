8d3cec90957f2f2bf684efee998c34e8ebd45f16
mihai <misu3108@yahoo.com>
2021-09-29 17:00:45 +0000

    small updates on steps

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,8 +5,8 @@ pipeline{
             steps{
               sh """
               #!/bin/bash
-              packer init .
-              packer build -force .
+              packer init -var-file=variables.pkr.hcl image.pkr.hcl
+              packer build -var-file=variables.pkr.hcl image.pkr.hcl
               """
             }
             
